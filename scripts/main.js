cd12a2ba68fb09afd19c4dea9d714441b2b5f01b
mlandrin <lan18015@byui.edu>
2022-04-23 00:26:03 -0600

    Updated Home Page

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+
+// Week 1 Assignment
+
+document.querySelector('img').classList.add('add_boarder');
+
+//document.querySelector('#lastmod').textContent =document.lastModified;
+
+let currentdate = document.lastModified;
+document.querySelector('#lastmod').textContent = currentdate;
+console.log(currentdate);
