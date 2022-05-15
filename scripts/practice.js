71ea5be254805920078573483260fa37648ccd35
mlandrin <lan18015@byui.edu>
2022-05-14 18:03:56 -0600

    commit

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,44 +1,57 @@
 // Variables and the date object
 
-let date = new Date();
-console.log(date);
+//let date = new Date();
+//console.log(date);
 
-let year = date.getFullYear();
-console.log(year);
+//let year = date.getFullYear();
+//console.log(year);
 
-document.querySelector('h1').textContent = year;
+//document.querySelector('h1').textContent = year;
 
-document.querySelector('div').innerHTML = '<h2>Subtitle</h2>';
+//document.querySelector('div').innerHTML = '<h2>Subtitle</h2>';
 
-let newpara = document.createElement('p');
-newpara.textContent = 'This is my paragraph';
-document.querySelector('div').appendChild(newpara);
+//let newpara = document.createElement('p');
+//newpara.textContent = 'This is my paragraph';
+//document.querySelector('div').appendChild(newpara);
 
-document.querySelector('img').setAttribute('width', '400');
+//document.querySelector('img').setAttribute('width', '400');
 
 //Sequence
 
-function addayear() {
-    console.log('it ran');
-}
-addayear(year);
+//function addayear() {
+  //  console.log('it ran');
+//}
+//addayear(year);
 
 // Repetition
 
-const myarray=['CIT110', 'CIT111', 'CIT230'];
-function makelist(item){
-    document.querySelector('div').innerHTML += '<li>' + item +'</li>'
-}
-myarray.forEach(makelist)
+//const myarray=['CIT110', 'CIT111', 'CIT230'];
+//function makelist(item){
+//    document.querySelector('div').innerHTML += '<li>' + item +'</li>'
+//}
+//myarray.forEach(makelist)
 
 
 // Selection
-if (year==2022) {
-    console.log('current year');
+//if (year==2022) {
+//    console.log('current year');
+//}
+
+
+//let date = new Date();
+//let year = date.getFullYear();
+
+//document.querySelector('#year')
+
+let button1 = document.querySelector('btn');
+
+function show(){
+button1.classList.add('newstyle');
 }
 
+button1.addEventListener('click', show);
 
-let date = new Date();
-let year = date.getFullYear();
+let hambutton = document.querySelector('.ham');
+letmainnav = document.querySelector('nav');
 
-document.querySelector('#year')
+hambutton.addEventListener('click', () => {mainnav.classList.toggle ('responsive')})
