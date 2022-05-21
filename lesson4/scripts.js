773ab96394ff84f8343e81b99e4aceffbe28989d
mlandrin <lan18015@byui.edu>
2022-05-20 23:24:01 -0600

    commit

--- lesson4/scripts.js
+++ lesson4/scripts.js
@@ -1,3 +1,9 @@
+let currentdate1 = document.currentdate;
+document.querySelector('#date').textContent = currentdate1;
+console.log(currentdate1);
+
+
 let currentdate = document.lastModified;
 document.querySelector('#lastmod').textContent = currentdate;
 console.log(currentdate);
+
