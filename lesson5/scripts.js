d415b5ad89c3691d9a9dc23b8205858e6488fb2e
mlandrin <lan18015@byui.edu>
2022-05-31 09:39:12 -0600

    commit

--- lesson5/scripts.js
+++ lesson5/scripts.js
@@ -18,3 +18,13 @@ const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('nav')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+const weekday = document.querySelector('.alert');
+if(weekday.getDay() == 2 || myDate.getDay() == 1) alert('🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.');
+
+//var weekday = new Date();
+//switch(date.getDay()){
+   // case 1: alert("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.");
+    //case 2: alert("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.");
+   // default: alert();}
